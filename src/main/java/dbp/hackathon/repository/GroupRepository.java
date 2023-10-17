992858caadb03457fceda3b7be77b1f65cb1c64e
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-10-16 22:07:21 -0500

    avacnes

--- src/main/java/dbp/hackathon/repository/GroupRepository.java
+++ src/main/java/dbp/hackathon/repository/GroupRepository.java
@@ -2,9 +2,8 @@ package dbp.hackathon.repository;
 
 import dbp.hackathon.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    List<Group> findAllByPersonsId(Long personId);
+    List<Group> findAllByMembers_Id(Long personId);  // Corrected derived query method
 }
